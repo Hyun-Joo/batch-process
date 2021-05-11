c97d6f9bb905fd5c946cf5277b2ad08b7c1fa802
Hyun-Joo <dlguswn714@gmail.com>
2021-05-11 21:30:17 +0900

    복습) 주석 추가 및 수정

--- src/main/java/com/batch/demo/part1/HelloConfiguration.java
+++ src/main/java/com/batch/demo/part1/HelloConfiguration.java
@@ -22,10 +22,23 @@ public class HelloConfiguration {
         this.stepBuilderFactory = stepBuilderFactory;
     }
 
+    /**
+     * JobInstance의 생성 기준은 JobParameter 중복 여부에 따라 생성
+     * 다른 parameter로 Job이 실행되면 JobInstance가 생성
+     * 같은 parameter로 Job이 실행되면 이미 생성된 JobInstance가 실행 
+     * JobExecution은 항상 새롭게 생성 
+     * ex)
+     * 	* 처음 실행 시 date parameter가 1월1일로 실행됐다면 1번 JobInstance 생성
+     * 	* 다음 실행 시 date parameter가 1월2일로 실행됐다면 2번 JobInstance 생성
+     * 	* 다음 실행 시 date parameter가 1월2일로 실행됐다면 2번 JobInstance 재실행
+     * 	=> Job이 재실행 대상이 아닌 경우 에러 발생
+     * Job) 항상 새로운 JobInstance가 실행될 수 있도록 RunIdIncrementer 제공
+     *  - RunIdIncrementer는 항상 다른 run.id를 parameter로 설정
+     */
     @Bean
     public Job helloJob(){
         return jobBuilderFactory.get("helloJob")
-                .incrementer(new RunIdIncrementer())
+                .incrementer(new RunIdIncrementer()) //항상 새로운 JobInstance를 만들도록 해줌 (id를 내부에서 자동 생성)
                 .start(this.helloStep())
                 .build();
     }
