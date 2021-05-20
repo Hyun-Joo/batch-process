cd4ad006681a5feea310e6e1fcbf83e8022b7cc1
Hyun-Joo <dlguswn714@gmail.com>
2021-05-20 20:53:39 +0900

    ItemWriter 예제 - csvFileItemWriter

--- src/main/resources/person.sql
+++ src/main/resources/person.sql
@@ -1,10 +1,10 @@
-CREATE TABLE person (
-    id BIGINT PRIMARY KEY AUTO_INCREMENT,
-    name VARCHAR(255),
-    age VARCHAR(255),
-    address VARCHAR(255)
-);
+-- create table person (
+--     id bigint primary key auto_increment,
+--     name varchar(255),
+--     age varchar(255),
+--     address varchar(255)
+-- );
 
-INSERT INTO person(name, age, address) VALUES('이현주', '31', '서울');
-INSERT INTO person(name, age, address) VALUES('홍길동', '30', '인천');
-INSERT INTO person(name, age, address) VALUES('아무새', '25', '부산');
+insert into person(name, age, address) values('이현주', '31', '서울');
+insert into person(name, age, address) values('홍길동', '30', '인천');
+insert into person(name, age, address) values('아무개', '25', '부산');
