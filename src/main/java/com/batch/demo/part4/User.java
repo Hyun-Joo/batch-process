99e17daad04682e1eb2a5864e785cb8839d150cd
Hyun-Joo <dlguswn714@gmail.com>
2021-06-17 12:55:42 +0900

    회원등급 배치 예제

--- src/main/java/com/batch/demo/part4/User.java
+++ src/main/java/com/batch/demo/part4/User.java
@@ -6,6 +6,7 @@ import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
 import java.time.LocalDate;
+import java.util.Objects;
 
 @Getter
 @Entity
@@ -31,6 +32,19 @@ public class User {
         this.totalAmount = totalAmount;
     }
 
+    public Level levelUp(){
+        Level nextLevel = Level.getNextLevel(this.getTotalAmount());
+
+        this.level = nextLevel;
+        this.updatedDate = LocalDate.now();
+
+        return nextLevel;
+    }
+
+    public boolean availableLevelUp() {
+        return Level.availableLevelUp(this.getLevel(), this.getTotalAmount());
+    }
+
     public enum Level {
         VIP(500_000, null),
         GOLD(500_000, VIP),
@@ -43,6 +57,34 @@ public class User {
         Level(int nextAmount, Level nextLevel) {
             this.nextAmount = nextAmount;
             this.nextLevel = nextLevel;
+        }
+
+        private static boolean availableLevelUp(Level level, int totalAmount) {
+            if(Objects.isNull(level)){
+                return false;
+            }
+            // nextLevel이 null인 경우는 VIP뿐임
+            if(Objects.isNull(level.nextLevel)){
+                return false;
+            }
+
+            return totalAmount >= level.nextAmount;
+        }
+
+        private static Level getNextLevel(int totalAmount) {
+            if(totalAmount >= Level.VIP.nextAmount){
+                return VIP;
+            }
+            if(totalAmount >= Level.GOLD.nextAmount){
+                return GOLD.nextLevel;
+            }
+            if(totalAmount >= Level.SILVER.nextAmount){
+                return SILVER.nextLevel;
+            }
+            if(totalAmount >= Level.NORMAL.nextAmount){
+                return NORMAL.nextLevel;
+            }
+            return NORMAL;
         }
     }
     
--- src/main/java/com/batch/demo/part4/UserRepository.java
+++ src/main/java/com/batch/demo/part4/UserRepository.java
@@ -1,6 +1,13 @@
 package com.batch.demo.part4;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+import java.time.LocalDate;
+import java.util.Collection;
+import java.util.Map;
+
+@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
+    Collection<Object> findAllByUpdatedDate(LocalDate now);
 }
